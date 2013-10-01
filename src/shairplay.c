f3271d5d1d52f2564b730df30c8a9787270a2c56
gleb <gleb@clusnode05>
2013-10-01 09:28:39 -0400

    Initial jack support

--- src/shairplay.c
+++ src/shairplay.c
@@ -21,11 +21,13 @@
  *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 #include <assert.h>
 
 #ifdef WIN32
@@ -36,6 +38,7 @@
 #include <shairplay/raop.h>
 
 #include <ao/ao.h>
+#include <jack/jack.h>
 
 #include "config.h"
 
@@ -305,6 +308,39 @@ parse_options(shairplay_options_t *opt, int argc, char *argv[])
 	return 0;
 }
 
+
+//---jack---
+
+jack_port_t *jack_output_port1, *jack_output_port2;
+jack_client_t *jack_client;
+
+/**
+ * The process callback for this JACK application is called in a
+ * special realtime thread once for each audio cycle.
+ *
+ * This client follows a simple rule: when the JACK transport is
+ * running, copy the input port to the output.  When it stops, exit.
+ */
+
+int
+process (jack_nframes_t nframes, void *arg)
+{   
+	return 0;      
+}
+
+/**
+ * JACK calls this shutdown_callback if the server ever shuts down or
+ * decides to disconnect the client.
+ */
+void
+jack_shutdown (void *arg)
+{
+	exit (1);
+}
+
+//--jack---
+
+
 int
 main(int argc, char *argv[])
 {
@@ -326,6 +362,108 @@ main(int argc, char *argv[])
 	if (parse_options(&options, argc, argv)) {
 		return 0;
 	}
+
+	//---jack---
+
+	const char **ports;
+	const char *client_name = "shairplay";
+	const char *server_name = NULL;
+	jack_options_t jack_options = JackNullOption;
+	jack_status_t jack_status;
+
+
+	jack_client = jack_client_open (client_name, jack_options, &jack_status, server_name);
+	if (jack_client == NULL) {
+		fprintf (stderr, "jack_client_open() failed, "
+			 "status = 0x%2.0x\n", jack_status);
+		if (jack_status & JackServerFailed) {
+			fprintf (stderr, "Unable to connect to JACK server\n");
+		}
+		exit (1);
+	}
+	if (jack_status & JackServerStarted) {
+		fprintf (stderr, "JACK server started\n");
+	}
+	if (jack_status & JackNameNotUnique) {
+		client_name = jack_get_client_name(jack_client);
+		fprintf (stderr, "unique name `%s' assigned\n", client_name);
+	}
+
+	/* tell the JACK server to call `process()' whenever
+	   there is work to be done.
+	*/
+
+	jack_set_process_callback (jack_client, process, 0);
+
+	/* tell the JACK server to call `jack_shutdown()' if
+	   it ever shuts down, either entirely, or if it
+	   just decides to stop calling us.
+	*/
+
+	jack_on_shutdown (jack_client, jack_shutdown, 0);
+
+	/* create two ports */
+
+	jack_output_port1 = jack_port_register (jack_client, "output1",
+					  JACK_DEFAULT_AUDIO_TYPE,
+					  JackPortIsOutput, 0);
+
+	jack_output_port2 = jack_port_register (jack_client, "output2",
+					  JACK_DEFAULT_AUDIO_TYPE,
+					  JackPortIsOutput, 0);
+
+	if ((jack_output_port1 == NULL) || (jack_output_port2 == NULL)) {
+		fprintf(stderr, "no more JACK ports available\n");
+		exit (1);
+	}
+
+	/* Tell the JACK server that we are ready to roll.  Our
+	 * process() callback will start running now. */
+
+	if (jack_activate (jack_client)) {
+		fprintf (stderr, "cannot activate client");
+		exit (1);
+	}
+
+	/* Connect the ports.  You can't do this before the client is
+	 * activated, because we can't make connections to clients
+	 * that aren't running.  Note the confusing (but necessary)
+	 * orientation of the driver backend ports: playback ports are
+	 * "input" to the backend, and capture ports are "output" from
+	 * it.
+	 */
+ 	
+	ports = jack_get_ports (jack_client, NULL, NULL,
+				JackPortIsPhysical|JackPortIsInput);
+	if (ports == NULL) {
+		fprintf(stderr, "no physical playback ports\n");
+		exit (1);
+	}
+
+	if (jack_connect (jack_client, jack_port_name (jack_output_port1), ports[0])) {
+		fprintf (stderr, "cannot connect output ports\n");
+	}
+
+	if (jack_connect (jack_client, jack_port_name (jack_output_port2), ports[1])) {
+		fprintf (stderr, "cannot connect output ports\n");
+	}
+
+	jack_free (ports);
+    
+    /* install a signal handler to properly quits jack client */
+#ifdef WIN32
+	signal(SIGINT, signal_handler);
+    signal(SIGABRT, signal_handler);
+	signal(SIGTERM, signal_handler);
+#else
+	signal(SIGQUIT, signal_handler);
+	signal(SIGTERM, signal_handler);
+	signal(SIGHUP, signal_handler);
+	signal(SIGINT, signal_handler);
+#endif
+	//---jack---
+	fprintf(stderr, "Jack initialized\n");
+
 
 	ao_initialize();
 
@@ -381,7 +519,7 @@ main(int argc, char *argv[])
 #endif
 	}
 
-	dnssd_unregister_raop(dnssd);
+	dnssd_unregister_raop(dnssd); 
 	dnssd_destroy(dnssd);
 
 	raop_stop(raop);
@@ -389,5 +527,6 @@ main(int argc, char *argv[])
 
 	ao_shutdown();
 
-	return 0;
+	jack_client_close (jack_client);
+	exit (0);
 }
